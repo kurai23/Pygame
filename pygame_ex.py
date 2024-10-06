4b4461cc8a21668eea79897f66a316468d408149
Robotsplayground <146990031+Robotsplayground@users.noreply.github.com>
2024-10-05 21:33:22 -0700

    Update pygame_ex.py
    Corrects Diagonal Movement and changed how the Car_Main.png is rotated

--- pygame_ex.py
+++ pygame_ex.py
@@ -9,10 +9,10 @@ running = True
 x,y=400,300
 
 
-Car = pygame.image.load(".\Images\Car.png")
-CarRight = pygame.image.load(".\Images\CarRight.png")
-CarLeft = pygame.image.load(".\Images\CarLeft.png")
-CarDown = pygame.image.load(".\Images\CarDown.png")
+Car = pygame.image.load(".\Images\Car_Main.png")
+#CarRight = pygame.image.load(".\Images\CarRight.png")
+#CarLeft = pygame.image.load(".\Images\CarLeft.png")
+#CarDown = pygame.image.load(".\Images\CarDown.png")
 
 
 car_image = Car        #Default image will be the car facing up
@@ -29,22 +29,47 @@ while running:
 
     # RENDER YOUR GAME HERE
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        y-=5
-        car_image = Car        #will change the image based on users input
-    if keys[pygame.K_DOWN]:
-        y+=5
-        car_image = CarDown
 
-    if keys[pygame.K_LEFT]:
-        x-=5
-        car_image = CarLeft
+    if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
 
-    if keys[pygame.K_RIGHT]:
-        x+=5
-        car_image = CarRight
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
 
-    screen.blit(car_image, (x, y))        #Adds the image of the car 
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                y-=3.5355
+                x-=3.5355
+                car_image = pygame.transform.rotate(Car , 45)
+            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                y-=3.5355
+                x+=3.5355
+                car_image = pygame.transform.rotate(Car , -45) #will change the image based on users input, BUT with pygame Rotate Func
+            else:
+                y-=5
+                car_image = Car #will change the image based on users input
+
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                y+=3.5355
+                x-=3.5355
+                car_image = pygame.transform.rotate(Car , 135)
+            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                y+=3.5355
+                x+=3.5355
+                car_image = pygame.transform.rotate(Car , 225)
+            else:
+                y+=5
+                car_image = pygame.transform.rotate(Car , 180)
+    else:
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            x-=5
+            car_image = pygame.transform.rotate(Car , 90)
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            x+=5
+            car_image = pygame.transform.rotate(Car , -90)
+
+    screen.blit(car_image, (x, y)) #Adds the image of the car 
 
 
 
